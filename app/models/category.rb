1d6dedb4c791b6576609d7a793d6ebe119d40884
Anica Lymus & Shannon Nolan & Youssef Kholeif <pair+anicalymus.shannonnolan17.yykholeif@devbootcamp.com>
2017-11-01 17:41:33 -0700

    Create show page for film

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ApplicationRecord
   has_many :film_categories
-  has_many :films, through: :film_categories
+  has_many :films, through: :film_categories, source: :film
   has_many :reviews, through: :films
 end
