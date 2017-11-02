bb3e757b34e5d269b7ed88b480b394bf804c6008
Youssef Kholeif <yykholeif@gmail.com>
2017-11-02 12:34:22 -0700

    rspect film model

--- spec/models/film_spec.rb
+++ spec/models/film_spec.rb
@@ -1,49 +1,50 @@
 require 'rails_helper'
 
 describe Film do
-  let(:film) { Film.new }
+  let(:film) { Film.create(title: "test") }
+  let(:category) { Category.create(name: "test") }
+  let(:user) { User.create(email: 'test@test.com', username:'test', password: '123')}
 
   describe "film reviews" do
     it "has many reviews" do
-      review = Review.create(id: 1, film: film, body: "awesome film")
+      review = Review.create(film: film, body: "awesome film", user_id: user.id)
         expect(film.reviews.first).to eq(review)
     end
 
   describe "film categories" do
     it "has many categories" do
-      category = Category.create(film: film, category: "drama")
-      expect(film.category.first).to eq(category)
+      film_category = FilmCategory.create(film: film, category: category)
+      expect(film.categories.first).to eq(category)
     end
   end
+
 
   describe "film comments" do
     it "has many comments" do
-      comment = Comment.create(film: film, comment: "superb!")
-      expect(film.comment.first).to eq(comment)
+      comment = Comment.create(user: user, body: "superb!")
+      expect(film.comments.first).to eq(comment)
     end
   end
 
-  describe "film ratings" do
-    it "has many ratings" do
-      rating = Rating.create(film: film, rating: 5)
-      expect(film.rating.first).to eq(5)
-    end
+#   describe "film ratings" do
+#     it "has many ratings" do
+#       rating = Rating.create(film: film, rating: 5)
+#       expect(film.rating.first).to eq(5)
+#     end
+#   end
+
+#   describe "film validations" do
+#     it "is valid when it has a title" do
+#       film.title = 'generic title'
+#       film.valid?
+#       expect(film.errors[:title]).to be_empty
+#     end
+
+#     it "is NOT valid when it has no title" do
+#       film.title = 'generic title'
+#       film.valid?
+#       expect(film.errors[:title]).to_not be_empty
+#     end
+#   end
   end
-
-  describe "film validations" do
-    it "is valid when it has a title" do
-      film.title = 'generic title'
-      film.valid?
-      expect(film.errors[:title]).to be_empty
-    end
-
-    it "is NOT valid when it has no title" do
-      film.title = 'generic title'
-      film.valid?
-      expect(film.errors[:title]).to_not be_empty
-    end
-  end
-end
-
-
 end
