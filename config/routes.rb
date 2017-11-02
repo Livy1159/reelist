08b5384da50e326331a601af6e7fe5acaada4e9e
Anica Lymus & Lorraine Hemenway & Olivia Morgan & Shannon Nolan & Youssef Kholeif <pair+anicalymus.flash00.livy1159.shannonnolan17.yykholeif@devbootcamp.com>
2017-11-01 17:42:01 -0700

    Add review controller, index view and route.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root 'films#index'
-  resources :films, only: [:index, :show]
+  resources :films, only: [:index, :show] do
+    resources :reviews, only: [:index]
+  end
   resources :users, only: [:create, :new]
+
 end
