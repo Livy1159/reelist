c0f4c3df8c27b37a9f64184634c51b790451f05e
anicalymus <anicalymus@gmail.com>
2017-11-03 12:24:14 -0700

    Add controller specs for testing

--- spec/controllers/films_controller_spec.rb
+++ spec/controllers/films_controller_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
-RSpec.describe Category, type: :model do
+RSpec.describe FilmsController, type: :controller do
 
+	describe "GET #index" do
+		it "has a 200 status code" do
+			get :index
+			expect(response.status).to eq(200)
+		end
+
+		it "renders the :index template" do
+			get :index
+			expect(response).to render_template(:index)
+		end
+	end
+	 describe "GET #show" do
+	 	context "finds a film by id" do
+	 		it "renders a film " do
+	 		expect(response). to render_template(:show)
+	 	end
+	 end
+ 
+    end		  
 end
